ad9d3488cddf59b864ee442d11ef7dd6671ad932
Andrey Koleshko <ka8725@gmail.com>
2012-01-16 01:20:44 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neday::Application.config.secret_token = '57fa866caded55889646c5a19f3915b1eafc4ba8bbddba76620f368e44c05eeec417a64672ee8dae6f19e4651b2105ff3deb89e48747330462cdd16095c58ea7'
