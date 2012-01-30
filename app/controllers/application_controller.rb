d7f18e89629f9f302ef48ee64e9415fb9488e3c1
Andrey Koleshko <ka8725@gmail.com>
2012-01-31 01:08:43 +0200

    twitter added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authentication_check
+
+  private
+  def authentication_check
+    return if Rails.env.development?
+    authenticate_or_request_with_http_basic do |user, password|
+      user == 'admin' && password == 'neday.org'
+    end
+  end
 end
