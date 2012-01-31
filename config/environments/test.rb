5a6adf090460a51db9b3c0712fdea4afbcb5ad55
Andrey Koleshko <ka8725@gmail.com>
2012-01-31 13:12:58 +0200

    Upgrade to rails 3.2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,5 @@ Neday::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  config.active_record.mass_assignment_sanitizer = :strict
 end
--- db/migrate/20120123224101_devise_create_users.rb
+++ db/migrate/20120123224101_devise_create_users.rb
@@ -1,16 +1,33 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
-      t.database_authenticatable :null => false
-      t.recoverable
-      t.rememberable
-      t.trackable
+      t.string :email
+      t.string :encrypted_password
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+      t.datetime :remember_created_at
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
-      # t.encryptable
-      # t.confirmable
-      # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
-      # t.token_authenticatable
+      ## Encryptable
+      # t.string :password_salt
 
+      ## Confirmable
+      # t.string   :confirmation_token
+      # t.datetime :confirmed_at
+      # t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
+
+      ## Lockable
+      # t.integer  :failed_attempts, :default => 0 # Only if lock strategy is :failed_attempts
+      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      # t.datetime :locked_at
+
+      # Token authenticatable
+      # t.string :authentication_token
 
       t.timestamps
     end
