a8c5900739bad9858aeeb7b84321fae0a7228ff7
Andrey Koleshko <ka8725@gmail.com>
2012-02-01 02:14:19 +0200

    Twitter fixed & fix for IE

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
-  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
-    data = access_token.extra.raw_info
-    if user = User.where(:email => data.email).first
+  def self.find_for_facebook_oauth(access_token, signed_in_resource = nil)
+    return signed_in_resource if signed_in_resource
+    data = access_token.info
+    if user = User.find_by_email(data.email)
       user
     else
+      # TODO: autofill first_name, last_name, avatar
       User.create!(:email => data.email, :password => Devise.friendly_token[0,20])
     end
   end
