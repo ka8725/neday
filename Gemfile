8bc0083d6513da87d305d9e224d09f4218315b5a
Andrey Koleshko <ka8725@gmail.com>
2012-02-06 14:50:55 +0200

    Twitter bootstrap attached

--- Gemfile
+++ Gemfile
@@ -17,10 +17,12 @@ gem 'settingslogic'
 gem 'carrierwave'
 gem 'rmagick'
 gem 'inherited_resources'
+gem 'simple-navigation'
 
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
+  gem 'twitter-bootstrap-rails'
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+      when :notice then 'info'
+      when :error then 'error'
+      when :alert then 'warning'
+    end
+  end
+
+  def flash_messages
+    res = ''
+    [:notice, :error, :alert].each do |level|
+      if flash[level].present?
+        res << %Q{<div data-alert="alert" class="alert alert-#{flash_class(level)} fade in">
+        <a class="close" data-dismiss="alert">&times;</a>
+        <p>#{flash[level]}</p>
+        </div>}.html_safe
+      end
+    end
+    res.html_safe
+  end
 end
