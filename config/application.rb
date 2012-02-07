5b85d96c80cc2b9efc86e8fbeed3d127054f7b79
Andrey Koleshko <ka8725@gmail.com>
2012-02-07 13:53:47 +0200

    User signup page by devise

--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,6 @@ module Neday
     config.generators do |g|
       g.test_framework :rspec, :fixture => true, :views => false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
-      g.template_engine :erubis
     end
-    end
+  end
 end
