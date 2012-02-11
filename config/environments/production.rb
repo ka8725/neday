19ad7933bfed9680df130b27cb6382659d06b82e
Andrey Koleshko <ka8725@gmail.com>
2012-02-11 22:03:47 +0300

    Production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,10 @@ Neday::Application.configure do
   # config.assets.compress = true
   #
   # # Don't fallback to assets pipeline if a precompiled asset is missed
-  # config.assets.compile = true
+  config.assets.compile = false
   #
   # # Generate digests for assets URLs
-  # config.assets.digest = true
+  config.assets.digest = true
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
@@ -26,13 +26,13 @@ Neday::Application.configure do
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  config.log_level = :debug
+  # config.log_level = :debug
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
