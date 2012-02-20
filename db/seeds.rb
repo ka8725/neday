a015607214545a52d7d54c30ba362dd4d3d81c5c
Andrey Koleshko <ka8725@gmail.com>
2012-02-21 01:57:31 +0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -9,3 +11,10 @@ require 'faker'
 require 'factory_girl_rails'
 Slide.destroy_all
 2.times { Factory(:slide) }
+
+ContactType.destroy_all
+ContactType.create!(:name => 'Skype', :icon => File.new(File.join(Rails.root, 'spec/images/skype.png')))
+ContactType.create!(:name => 'Мобильный телефон', :icon => File.new(File.join(Rails.root, 'spec/images/mobile_phone.png')))
+ContactType.create!(:name => 'Домашний телефон', :icon => File.new(File.join(Rails.root, 'spec/images/phone.png')))
+ContactType.create!(:name => 'ICQ', :icon => File.new(File.join(Rails.root, 'spec/images/ICQ.png')))
+ContactType.create!(:name => 'Jabber', :icon => File.new(File.join(Rails.root, 'spec/images/jabber.png')))
