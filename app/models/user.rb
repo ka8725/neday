f5b20db29fe04c8c273b8ea0bb8c5dcf93aa7c9e
Andrey Koleshko <ka8725@gmail.com>
2012-03-01 00:55:05 +0300

    Edit user sports edit and show sports

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :facebook_id, :avatar,
                   :remote_avatar_url, :remove_avatar, :avatar_cache, :vkontakte_id, :location_attributes, :twitter_id, :google_id,
-                  :contacts_attributes, :user_sports
+                  :contacts_attributes, :user_sports_attributes
   has_one :location, :as => :locationable
   accepts_nested_attributes_for :location
 
