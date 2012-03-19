c90305d28f9327535e0097ecfe9cc67dc2e4d524
Andrey Koleshko <ka8725@gmail.com>
2012-03-19 20:47:35 +0300

    fix travis

--- db/migrate/20120206153117_move_admin_notes_to_comments.rb
+++ db/migrate/20120206153117_move_admin_notes_to_comments.rb
@@ -9,8 +9,10 @@ class MoveAdminNotesToComments < ActiveRecord::Migration
     add_index     :active_admin_comments, [:author_type, :author_id]
 
     # Update all the existing comments to the default namespace
-    say "Updating any existing comments to the #{ActiveAdmin.application.default_namespace} namespace."
-    execute "UPDATE active_admin_comments SET namespace='#{ActiveAdmin.application.default_namespace}'"
+    if defined?(ActiveAdmin)
+      say "Updating any existing comments to the #{ActiveAdmin.application.default_namespace} namespace."
+      execute "UPDATE active_admin_comments SET namespace='#{ActiveAdmin.application.default_namespace}'"
+    end
   end
 
   def self.down
