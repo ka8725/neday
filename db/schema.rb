49f196953492ff99ab12a2b6a03519265cf6798c
Victor <oakypinky@oakypinky-VirtualBox.(none)>
2012-05-16 15:32:13 +0300

    note migration at event creating removed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120315000347) do
+ActiveRecord::Schema.define(:version => 20120516120503) do
 
   create_table "contact_types", :force => true do |t|
     t.string   "name"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(:version => 20120315000347) do
     t.integer  "place_id"
     t.integer  "sport_id"
     t.integer  "max_number_members"
-    t.text     "note"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
   end
