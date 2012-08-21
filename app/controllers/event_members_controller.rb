67dc4d9e7dbcc17a8f7d7317137bebb92f2c8b41
Andrey Koleshko <ka8725@gmail.com>
2012-08-21 18:21:39 +0300

    fix notice for event members controller

--- app/controllers/event_members_controller.rb
+++ app/controllers/event_members_controller.rb
@@ -1,13 +1,14 @@
+# -*- encoding : utf-8 -*-
 class EventMembersController < ApplicationController
 
   def create
     Event.find(params[:event_id]).members << current_user
-    redirect_to :back
+    redirect_to :back, :notice => 'Вы идете на событие!'
   end
 
   def destroy
     event = Event.find(params[:id])
     event.members.delete(current_user)
-    redirect_to :back
+    redirect_to :back, :notice => 'Вы больше не участвуете в этом событии'
   end
 end
