bcb3fe977a7692569506d8770fbc4fc0c7a94d04
Andrey Koleshko <ka8725@gmail.com>
2012-09-09 23:09:36 +0300

    spec for event owner presence

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe Event do
   let (:event) { FactoryGirl.create(:event) }
 
+  it { should have_db_index(:owner_id) }
+  it { should have_db_index(:sport_id) }
+  it { should have_db_index(:place_id) }
+
   context :associations do
     it { should belong_to(:place) }
     it { should belong_to(:owner) }
@@ -12,6 +16,7 @@ describe Event do
   context :validations do
     it { should validate_presence_of(:sport) }
     it { should validate_presence_of(:place) }
+    it { should validate_presence_of(:owner_id) }
     it { should validate_presence_of(:start_at) }
   end
 
