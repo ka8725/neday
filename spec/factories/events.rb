93f4fa7adf6e8ec532c61ad73cf3280bedbad797
Victor V. Voinilovich <v.voinilvich@gmail.com>
2012-09-23 22:29:13 +0300

    fix tests and model for event

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,10 +3,8 @@
 FactoryGirl.define do
   factory :event do
     association           :owner, :factory => :user
-    duration              { rand(10) }
     start_at              10.days.since
     association           :place
     association           :sport
-    max_number_members    { rand(10) + 1 }
   end
 end
